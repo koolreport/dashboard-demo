6d8bb9ca5d58c8f1ef2d989bc8abed8dcd29726b
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2021-07-05 16:46:40 +0700

    Add explanation to TStatePersisted

--- src/customers/CustomerListBoard.php
+++ src/customers/CustomerListBoard.php
@@ -9,6 +9,10 @@ use \koolreport\dashboard\containers\Html;
 
 class CustomerListBoard extends Dashboard
 {
+    // Include the TStatePersisted to make Dashboard state persisted
+    // so when we come back to Dashboard again, it will resume to
+    // exact state when we left, all selection, table paging will
+    // be remained the same.
     use \koolreport\dashboard\TStatePersisted;
 
     protected function widgets()
