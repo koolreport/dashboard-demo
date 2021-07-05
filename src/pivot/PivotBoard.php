a3b7bbaddf07aab0e3a97753ccdede3ffd881a2c
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2021-07-05 14:49:10 +0700

    Change Text to Html

--- src/pivot/PivotBoard.php
+++ src/pivot/PivotBoard.php
@@ -3,26 +3,17 @@
 namespace demo\pivot;
 
 use \koolreport\dashboard\Dashboard;
-<<<<<<< HEAD
-use \koolreport\dashboard\containers\Row;
-use \koolreport\dashboard\containers\Panel;
-use \koolreport\dashboard\widgets\Text;
-=======
-
 use \koolreport\dashboard\containers\Row;
 use \koolreport\dashboard\containers\Panel;
 use \koolreport\dashboard\containers\Html;
->>>>>>> 3a9eb5706675c586261649710c524751c443e41e
 
 class PivotBoard extends Dashboard
 {
     protected function widgets()
     {
         return [
-<<<<<<< HEAD
             Panel::create()->type("secondary")->header("<b>What is Pivot widgets?</b>")->sub([
-                Text::create()
-                ->text("
+                Html::p("
                     Pivot widgets include PivotTable and PivotMatrix. PivotTable is a fixed displaying widget for pivot data while PivotMatrix is a dynamic widget which allows users to drag and drop pivot fields, do sorting, etc.
                 ")
             ]),
@@ -36,9 +27,6 @@ class PivotBoard extends Dashboard
                 CustomersPivotMatrix::create(),
             ]),
 
-=======
-            Html::h1("Pivot")
->>>>>>> 3a9eb5706675c586261649710c524751c443e41e
         ];
     }
 }
