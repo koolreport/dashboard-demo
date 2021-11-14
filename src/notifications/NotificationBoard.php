13ad86ab95804de2ec24bbf015074082862182b4
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2021-11-15 02:26:25 +0700

    Notification board

--- src/notifications/NotificationBoard.php
+++ src/notifications/NotificationBoard.php
@@ -21,7 +21,8 @@ class NotificationBoard extends Dashboard
             ->header("Notifications"),
 
             \demo\CodeDemo::create("
-                This example shows you how to show type of notifications: Alert, Confirm and Note 
+                This example shows you how to show type of notifications: Alert, Confirm and Note.
+                The notification object is returned in action method of dashboard or widget.
             ")->raw(true)
         ];
     }
