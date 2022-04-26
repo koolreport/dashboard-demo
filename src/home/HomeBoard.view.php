675ead0f669a49a52443d509487a6562d5942fe0
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2022-04-26 09:52:42 +0700

    adding promotion

--- src/home/HomeBoard.view.php
+++ src/home/HomeBoard.view.php
@@ -1,11 +1,11 @@
 <div class="homeboard">
-    <div class="mb-3" style="display:none;">
-        <a  title="Get KoolReport Pro with 40% OFF." 
-            href="https://www.koolreport.com/koolreport-pro-pricing#choose-your-license" 
-            style="padding:0px;text-decoration: none;">
+    <div class="mb-3">
+        <a  title="Big Discount 40% for KoolReport Pro" 
+            href="https://www.koolreport.com/get-koolreport-pro?pcode=big#now" 
+            style="background-color:#1f1f1f;color:#c90000;padding:0px;text-decoration: none;">
             <img class="img-fluid" 
-                style="border-radius:4px;width:100%;"
-                src="https://www.koolreport.com/assets/images/editor/c5/image619f517ec353d.png" />
+                style="border-radius:4px"
+                src="https://www.koolreport.com/assets/images/editor/c5/image62675ab7c7571.png" />
         </a>
     </div>
     <div class="card">
