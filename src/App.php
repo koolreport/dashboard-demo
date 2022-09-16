1ec3f37326a98f7b365fa004e1cc599bfa79d619
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2022-09-17 03:58:54 +0700

    Adding themes, languages, pages and inline editing

--- src/App.php
+++ src/App.php
@@ -2,18 +2,28 @@
 
 namespace demo;
 
-use \koolreport\dashboard\menu\Section;
+
 use \koolreport\dashboard\menu\Group;
 use \koolreport\dashboard\pages\Login;
 use \koolreport\dashboard\User;
 
-use \koolreport\dashboard\menu\MenuItem;
 use \koolreport\dashboard\Client;
 use \koolreport\dashboard\ExportHandler;
 use \koolreport\dashboard\export\ChromeHeadlessio;
 use koolreport\dashboard\export\CSVEngine;
 use koolreport\dashboard\export\XLSXEngine;
+
 use koolreport\dashboard\menu\MegaMenu;
+use \koolreport\dashboard\menu\MenuItem;
+
+use koolreport\amazing\dashboard\Amazing;
+use koolreport\appstack\dashboard\AppStack;
+use koolreport\dashboard\Cookie;
+use koolreport\dashboard\languages\DE;
+use koolreport\dashboard\languages\EN;
+use koolreport\dashboard\languages\FR;
+use koolreport\dashboard\languages\TH;
+use koolreport\dashboard\languages\VN;
 
 class App extends \koolreport\dashboard\Application
 {
@@ -36,6 +46,10 @@ class App extends \koolreport\dashboard\Application
         ")
         ->js("//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.0.1/highlight.min.js")
         ->css("https://cdn.koolreport.com/examples/assets/theme/tomorrow.css");
+
+        $themeName = Cookie::themeName();
+        $themeName = ($themeName!==null)?$themeName:"Amazing";
+        $this->setTheme($themeName);
     }
 
     protected function login()
@@ -71,26 +85,55 @@ class App extends \koolreport\dashboard\Application
                 });
     }
 
+    /**
+     * Provide list of languages for user to choose
+     * @return array 
+     */
+    protected function languages()
+    {
+        return [
+            EN::create(),
+            DE::create(),
+            FR::create(),
+            TH::create(),
+        ];
+    }
 
+
+    /**
+     * Return list of all pages.
+     * Here will have two pages: PublicPage which user can access freely
+     * and MemberPage which will required user to login access
+     * @return array 
+     */
     protected function pages()
     {
         return [
             PublicPage::create()->loginRequired(false),
-            MemberPage::create(),
+            MemberPage::create()->loginRequired(true),
         ];
     }
 
+    /**
+     * Return the top menu. By providing the method on app,
+     * we will use the same top menu on every pages
+     * @return array 
+     */
     protected function topMenu()
     {
         return [
             "Mega Menu"=>MegaMenu::create()->sub([
-                "Page List"=>Group::create()->sub([
-                    "Public Page"=>MenuItem::create()->onClick(Client::navigate("App/PublicPage")),
-                    "Member Page"=>MenuItem::create()->onClick(Client::navigate("App/MemberPage")),
+                "Pages"=>Group::create()->sub([
+                    "Public"=>MenuItem::create()
+                        ->icon("fa fa-globe")
+                        ->onClick(Client::navigate("App/PublicPage")),
+                    "Member"=>MenuItem::create()
+                        ->icon("fa fa-lock")
+                        ->onClick(Client::navigate("App/MemberPage")),
                 ]),
                 "Themes"=>Group::create()->sub([
-                    "Amazing Theme"=>MenuItem::create()->onClick(Client::navigate("App/PublicPage")),
-                    "AppStack Theme"=>MenuItem::create()->onClick(Client::navigate("App/PublicPage")),
+                    "Amazing"=>MenuItem::create()->onClick(Client::app()->action("changeTheme",["name"=>"Amazing"])),
+                    "AppStack"=>MenuItem::create()->onClick(Client::app()->action("changeTheme",["name"=>"AppStack"])),
                 ])
             ]),
             "About"=>MenuItem::create()
@@ -102,6 +145,45 @@ class App extends \koolreport\dashboard\Application
         ];
     }
 
+    /**
+     * This action will get the user request of changing theme from client
+     * it will set the theme as well as save the current theme selection
+     * to cookie. Last, it will reload page to get theme changed.
+     * @param mixed $request 
+     * @param mixed $response 
+     * @return void 
+     */
+    protected function actionChangeTheme($request, $response)
+    {
+        $themeName = $request->params("name");
+        $this->setTheme($themeName);
+        Cookie::themeName($themeName);
+        $response->runScript("location.reload()");
+    }
+
+    /**
+     * Just a function to set correct theme for App
+     * from the name of theme
+     * @param mixed $name 
+     * @return void 
+     */
+    protected function setTheme($name)
+    {
+        switch($name) {
+            case "Amazing":
+                $this->theme(Amazing::create());
+                break;
+            case "AppStack":
+                $this->theme(AppStack::create());
+                break;
+        }
+    }
+
+    /**
+     * Account menu that will appear when user login
+     * We will use the same account menu on all pages
+     * @return array 
+     */
     protected function accountMenu()
     {
         return [
