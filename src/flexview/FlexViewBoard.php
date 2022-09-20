e8cf291bf9c8b65d4d627db8ac9077cf8b436ad1
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2022-09-21 04:54:01 +0700

    Change layout of flexview example

--- src/flexview/FlexViewBoard.php
+++ src/flexview/FlexViewBoard.php
@@ -15,55 +15,63 @@ class FlexViewBoard extends Dashboard
     protected function content()
     {
         return [
-            Panel::create()->type("primary")->header(Html::h5("Flexview - Turn flex view into a drilldown"))->sub([
-                FlexView::create("myFlexView")
-                ->viewOptions([
-                    "customers"=>function($params) {
-                        return [
-                            Row::create([
-                                Html::h3("Customers")
-                            ]),
+            FlexView::create("myFlexView")
+            ->viewOptions([
+                "customers"=>function($params) {
+                    return [
+                        Row::create([
+                            Html::h3("Customers")
+                        ]),
+                        Panel::create()->sub([
+                            Html::p(Html::i("*Note: Click to row to view orders of a customer")),
                             CustomerTable::create()
-                        ];
-                    },
-                    "orders"=>function($params) {
-                        //Params contain "customerNumber" and "customerName"
-                        $customerNumber = $params["customerNumber"];
-                        $customerName = $params["customerName"];
-                        return [
-                            Row::create([
-                                Html::h3("Orders of $customerName(#$customerNumber)"),
-                                Html::div()->class("text-right")->sub([
-                                    Button::create("backButton")
-                                    ->text(Lang::t("Back"))
-                                    ->action("submit",function($request, $response){
-                                        $this->sibling("myFlexView")->showView("customers");
-                                    }),
-                                ]) 
-                            ]),
+                        ])
+                    ];
+                },
+                "orders"=>function($params) {
+                    //Params contain "customerNumber" and "customerName"
+                    $customerNumber = $params["customerNumber"];
+                    $customerName = $params["customerName"];
+                    return [
+                        Row::create([
+                            Html::h3("Orders of $customerName(#$customerNumber)"),
+                            Html::div()->class("text-right")->sub([
+                                Button::create("backButton")
+                                ->text(Lang::t("Back"))
+                                ->action("submit",function($request, $response){
+                                    $this->sibling("myFlexView")->showView("customers");
+                                }),
+                            ]) 
+                        ]),
+                        Panel::create()->sub([
+                            Html::p(Html::i("*Note: Click to row to view details of an order")),
                             OrderTable::create()->params($params)
-                        ];
-                    },
-                    "orderdetails"=>function($params) {
-                        $orderNumber = $params["orderNumber"];
-                        $customerName = $params["customerName"];
-                        return [
-                            Row::create([
-                                Html::h3("Detail of order #$orderNumber of $customerName"),
-                                Html::div()->class("text-right")->sub([
-                                    Button::create("backButtonOrderDetail")
-                                    ->text(Lang::t("Back"))
-                                    ->action("submit",function($request, $response) use ($params){
-                                        $this->sibling("myFlexView")->showView("orders",$params);
-                                    }),
-                                ]) 
-                            ]),
+                        ])
+                    ];
+                },
+                "orderdetails"=>function($params) {
+                    $orderNumber = $params["orderNumber"];
+                    $customerName = $params["customerName"];
+                    return [
+                        Row::create([
+                            Html::h3("Detail of order #$orderNumber of $customerName"),
+                            Html::div()->class("text-right")->sub([
+                                Button::create("backButtonOrderDetail")
+                                ->text(Lang::t("Back"))
+                                ->action("submit",function($request, $response) use ($params){
+                                    $this->sibling("myFlexView")->showView("orders",$params);
+                                }),
+                            ]) 
+                        ]),
+                        Panel::create()->sub([
+                            Html::p(Html::i("*Note: Click back button to go back to previous table")),
                             OrderDetailTable::create()->params($params)
-                        ];
-                    }
-                ])
-                ->initialView("customers")
-            ]),
+                        ])
+                    ];
+                }
+            ])
+            ->initialView("customers"),
+
             \demo\CodeDemo::create("
                     The example show you how to use FlexView to create a simple drilldown. By clicking to row of table, you will go to details
                     of sub table.
