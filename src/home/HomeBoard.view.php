0d29076e642bb5b679520a7b9cc3e326544aa3ea
Nghiem Anh Tuan <nghiemanhtuan@gmail.com>
2022-11-08 15:04:38 +0700

    remove ads

--- src/home/HomeBoard.view.php
+++ src/home/HomeBoard.view.php
@@ -1,11 +1,4 @@
 <div class="homeboard">
-    <div class="mb-3">
-        <a  title="Get 30 % OFF for KoolReport Pro and Dashboard Framework" 
-            href="https://www.koolreport.com/koolreport-pro-pricing?pcode=halloween#choose-your-license" 
-            style="padding:0px;text-decoration: none;">
-            <img class="img-fluid rounded" src="https://www.koolreport.com/assets/images/editor/c5/image63578c7f0f999.png"/>
-        </a>
-    </div>    
     <div class="card">
         <div class="card-body">
             <h1>Introduction</h1>
